6e7e86e1c56046c508280db0ae3811c6deb6ede9
Alan <alan35691@gmail.com>
2024-05-07 10:59:47 +0100

    Project created

---
+++ connecting-kerry/src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+//div.classname and press tab for a shortcut, div# for id
+// all functions have to be capital letter
+
+
+function Navbar() {
+  return (
+    <div>
+        <div className="navbar">HEELLLLLLLLLLLLLLLLLOOOO</div>
+        <div id="login">login </div>
+    </div>
+  )
+}
+
+export default Navbar;
